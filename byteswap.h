5238cbd13212b6e6888286421145d75ec3caac91
Elviss Strazdins <elviss@elviss.lv>
2019-03-12 03:36:13 +0200

    Add copyright to the byteswap.h

--- byteswap.h
+++ byteswap.h
@@ -1,5 +1,19 @@
+/* Copyright (C) 1997-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
 #ifndef _BYTESWAP_H
-#define _BYTESWAP_H
+#define _BYTESWAP_H	1
 
 #include <stdint.h>
 
@@ -18,8 +32,16 @@ static __inline uint64_t __bswap_64(uint64_t __x)
 	return __bswap_32(__x)+0ULL<<32 | __bswap_32(__x>>32);
 }
 
-#define bswap_16(x) __bswap_16(x)
-#define bswap_32(x) __bswap_32(x)
-#define bswap_64(x) __bswap_64(x)
+/* The following definitions must all be macros since otherwise some
+   of the possible optimizations are not possible.  */
 
-#endif
+/* Return a value with all bytes in the 16 bit argument swapped.  */
+#define bswap_16(x) __bswap_16 (x)
+
+/* Return a value with all bytes in the 32 bit argument swapped.  */
+#define bswap_32(x) __bswap_32 (x)
+
+/* Return a value with all bytes in the 64 bit argument swapped.  */
+#define bswap_64(x) __bswap_64 (x)
+
+#endif /* byteswap.h */
